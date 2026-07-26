474a7cf878766f182288466f52429c04f251ce69
copycat77842 <copycat77842@gmail.com>
2026-07-27 07:27:04 +1000

    Update manifest.js
    Added investigation style

--- data/manifest.js
+++ data/manifest.js
@@ -6,10 +6,10 @@
 // To add a new case in the future, add one entry per age variant here and
 // drop the matching file in data/.
 window.CYBERTRAIL_SCENARIO_SOURCES = [
-  { id: "password_student", caseId: "password", ageGroup: "student", src: "data/password_student.js" },
-  { id: "password_adult", caseId: "password", ageGroup: "working-adult", src: "data/password_adult.js" },
-  { id: "password_olderadult", caseId: "password", ageGroup: "older-adult", src: "data/password_olderadult.js" },
-  { id: "phishing_student", caseId: "phishing", ageGroup: "student", src: "data/phishing_student.js" },
-  { id: "phishing_adult", caseId: "phishing", ageGroup: "working-adult", src: "data/phishing_adult.js" },
-  { id: "phishing_olderadult", caseId: "phishing", ageGroup: "older-adult", src: "data/phishing_olderadult.js" }
+  { id: "password_student", caseId: "password", ageGroup: "student", style: "Case Files", src: "data/password_student.js" },
+  { id: "password_adult", caseId: "password", ageGroup: "working-adult", style: "Case Files", src: "data/password_adult.js" },
+  { id: "password_olderadult", caseId: "password", ageGroup: "older-adult", style: "Case Files", src: "data/password_olderadult.js" },
+  { id: "phishing_student", caseId: "phishing", ageGroup: "student", style: "Artifact Analysis", src: "data/phishing_student.js" },
+  { id: "phishing_adult", caseId: "phishing", ageGroup: "working-adult", style: "Artifact Analysis", src: "data/phishing_adult.js" },
+  { id: "phishing_olderadult", caseId: "phishing", ageGroup: "older-adult", style: "Artifact Analysis", src: "data/phishing_olderadult.js" }
 ];
